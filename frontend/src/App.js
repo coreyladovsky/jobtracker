34f7bdd80b046e9b3bb4e88c78891736615c3a6d
Corey Ladovsky <coreyskymusic@gmail.com>
2020-07-08 17:41:40 -0400

    skeleton

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route } from 'react-router-dom'
+import Home from './Home';
+import SignUp from './SignUp';
+import ForgotPassword from './ForgotPassword';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route exact path="/">
+          <Home />
+      </Route>
+      <Route path="/signup">
+          <SignUp />
+      </Route>
+      <Route path="/forgot-password">
+        <ForgotPassword />
+      </Route>
     </div>
   );
 }
---
+++ frontend/src/JobsIndexItem.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+
+export default (params) => {
+    return null;
+}
