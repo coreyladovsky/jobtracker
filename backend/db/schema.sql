bd4effb427aee82a93739ea58cef60d33f9c42eb
Corey Ladovsky <coreyskymusic@gmail.com>
2020-07-09 16:21:20 -0400

    add backend skeleton

---
+++ backend/db/schema.sql
@@ -0,0 +1,22 @@
+
+-- DROP DATABASE IF EXISTS jobtracker;
+-- CREATE DATABASE jobtracker;
+
+-- \c jobtracker;
+
+
+DROP TABLE IF EXISTS jobs;
+CREATE TABLE jobs
+(
+    id SERIAL PRIMARY KEY,
+    company VARCHAR,
+    job_title VARCHAR,
+    post_url VARCHAR,
+    salary VARCHAR,
+    location VARCHAR,
+    due_date VARCHAR,
+    description VARCHAR,
+    created_at TIMESTAMP
+    WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    user_id VARCHAR
+    );
